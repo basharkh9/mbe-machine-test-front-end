2f14c7c898a31843e4850931d6efa2244a6205eb
Bashar Khadra <bashar.work.92@gmail.com>
2022-06-06 17:47:17 +0400

    Added DataService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataServiceService } from './services/data-service.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [DataServiceService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/services/data-service.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  // this comment added by basharkh9
+  // since Angular 9 you can provided in 'root', 'any', 'platform'
+  providedIn: 'root',
+})
+export class DataServiceService {
+  constructor(private httpClient: HttpClient) {}
+
+  // this method return an observable wich is lazy loaded and not the actual data,
+  // hence you have to subscribed to it
+  // store the subscribtion
+  // and unsubscribe on destroying the component.
+  getData() {
+    return this.httpClient.get('assets/db.json');
+  }
+}
