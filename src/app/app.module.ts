75e402ed2dd7d3b7bbc9864357d7804dbf686e6e
Bashar Khadra <bashar.work.92@gmail.com>
2022-06-06 21:02:05 +0400

    Added shared card component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DataServiceService } from './services/data-service.service';
+import { MovieGridComponent } from './movie-grid/movie-grid.component';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MovieGridComponent, CardComponent],
   imports: [BrowserModule],
   providers: [DataServiceService],
   bootstrap: [AppComponent],
