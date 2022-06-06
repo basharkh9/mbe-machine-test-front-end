0731bb397724048ec0266bc282728cbf7c14b757
Bashar Khadra <bashar.work.92@gmail.com>
2022-06-07 02:28:04 +0400

    Configure routing and use <router-outlet>.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DataService } from './services/data.service';
@@ -7,9 +8,11 @@ import { MovieGridComponent } from './movie-grid/movie-grid.component';
 import { CardComponent } from './shared/card/card.component';
 import { HttpClientModule } from '@angular/common/http';
 
+const appRoute: Routes = [{ path: '', component: MovieGridComponent }];
+
 @NgModule({
   declarations: [AppComponent, MovieGridComponent, CardComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoute)],
   providers: [DataService],
   bootstrap: [AppComponent],
 })
